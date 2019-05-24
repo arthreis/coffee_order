d6df38b7ef3a2b8bbdd25c93c3c43bd1b5d6f0b4
arthur.reis <arthur.reis@radixeng.com.br>
2019-05-24 15:29:49 -0300

    inclusão das imagens dos produtos

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class Product {
   final String name;
   final double price;
+  final String image;
 
   Product({
     @required this.name,
-    @required this.price
+    @required this.price,
+    @required this.image,
   });
 }
